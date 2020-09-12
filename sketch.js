55c58b3a091e43cda6a3f99f3e89e4daaeb7a1bb
Sara Do <sara@Jiui-MacBook-Pro.local>
2020-09-11 23:24:35 -0400

    First sketch

--- sketch.js
+++ sketch.js
@@ -1,8 +1,26 @@
+let xpos = 60;
+let ypos = 17;
+
+let xpos2 = 150;
+let ypos2 = 250;
+
 function setup() {
   // put setup code here
-background(100);
+  createCanvas(500,500);
+
 }
 
 function draw() {
+    background(200,300,200);
   // put drawing code here
+    translate(xpos,ypos);
+    fill(0,100,0);
+    rect(xpos,ypos,150,100);
+    xpos = xpos - 3;
+    ypos = ypos + 5;
+
+    circle(xpos2,ypos2,130);
+    xpos2 = xpos2 + 15;
+    ypos2 = ypos2 + 5;
+
 }
