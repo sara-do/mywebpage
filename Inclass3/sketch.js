7ddb8a20702d0871c960f9119db8084ec9a869f3
Sara Do <sara@Jiui-MacBook-Pro.local>
2020-09-24 15:41:14 -0400

    in-class exercise #3

--- Inclass3/sketch.js
+++ Inclass3/sketch.js
@@ -1,15 +1,30 @@
 
 function setup() {
   // put setup code here
-createCanvas(200,200);
-background(200,10,120);
+createCanvas(windowWidth,windowHeight);
+background(150,200,150);
 
 }
 
 function draw() {
   //Put draw code here
-  fill(30,40,200);
-  circle(100,100,70);
+  for (let d = 0; d < width; d = d + 35){
+    for (let c = 0; c < height; c = c + 35){
 
+    if(c == d){
 
+    }
+    else{
+      circle(c,d,40);
+      fill(50,100,50);
+    }
+  }
+}
+
+   for(let d = 0; d < height; d = d + 10){
+     let xlen = random(0,width/2);
+     strokeWeight (3);
+     stroke(150,100,70);
+     line(2000,d,xlen,d);
+   }
 }
