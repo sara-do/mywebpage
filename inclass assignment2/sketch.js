442e1d77a1ff5c2a038228048c5113d0007bb7a1
Sara Do <sara@Jiui-MacBook-Pro.local>
2020-09-24 10:55:34 -0400

    Added new assignment #2

---
+++ inclass assignment2/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth,windowHeight);
+
+}
+
+function draw() {
+  //Put draw code here
+  background(200,10,120);
+  fill(30,40,200);
+  rect(300,100,400);
+
+
+}
