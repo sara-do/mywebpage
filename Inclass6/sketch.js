0e1a9c9f02fdc4de5903dccba839a1f9d4200f94
Sara Do <sara@Jiui-MacBook-Pro.local>
2020-10-24 04:28:32 +0900

    In class assignment 6

--- Inclass6/sketch.js
+++ Inclass6/sketch.js
@@ -1,42 +1,80 @@
-let xposList = [100,500,700,900,1000];
-let yposList = [400,300,500,200,100];
-let yspeedList = [13,12,10,6,8];
-let xspeedList = [2,3,1,5,4];
+let img1;
+let img2;
+let img3;
+
+function preload(){
+  img1 = loadImage('https://i.imgur.com/d6hfkYD.jpg');
+  img2 = loadImage('https://i.imgur.com/d6hfkYD.jpg');
+  img3 = loadImage('https://i.imgur.com/d6hfkYD.jpg');
+}
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
+  background(20,100,150);
+
 }
 
 function draw() {
-    background(10,65,120);
+  img1.loadPixels();
 
-    for(let i = 0; i < xposList.length ; i = i + 1){
+  image(img1,0,0);
 
-      circle(xposList[i],yposList[i],50);
+  for(let x = 0; x < img1.width; x = x + 1){
+   for(let y = 0; y < img1.height; y = y + 1){
+      let i = (x + y*img1.width)*4;
 
-      yposList[i] = yposList[i] + yspeedList[i];
+      let pixr = img1.pixels[i+0];
+      let pixg = img1.pixels[i+1];
+      let pixb = img1.pixels[i+2];
+      let pixa = img1.pixels[i+3];
 
-      if(yposList[i] > height){
-        yspeedList[i] = -yspeedList[i];
-      }
-
-      if(yposList[i] < 0){
-        yspeedList[i] = - yspeedList[i];
-      }
-
-   for(let i = 0; i < yposList.length ; i = i + 1){
-     xposList[i] = xposList[i] + xspeedList[i];
-
-     if(xposList[i] > width){
-       xspeedList[i] = -xspeedList[i];
-     }
-
-     if(xposList[i] < 0){
-       xspeedList[i] = - xspeedList[i];
-     }
+      img1.pixels[i+1] = 0;
 
    }
+ }
 
-    }
+img1.updatePixels();
 
+ img2.loadPixels();
+
+ image(img2,420,0);
+
+ for(let x = 0; x < img2.width; x = x + 10){
+  for(let y = 0; y < img2.height; y = y + 10){
+     let i = (x + y*img2.width)*4;
+
+     let pixr = img2.pixels[i+0];
+     let pixg = img2.pixels[i+1];
+     let pixb = img2.pixels[i+2];
+     let pixa = img2.pixels[i+3];
+
+
+      circle(x+425,y,10);
+      fill(pixr,pixg,pixb,pixa);
+
+ }
+}
+
+img2.updatePixels();
+
+
+  img3.loadPixels();
+
+  image(img3,840,0);
+
+  for(let x = 0; x < img3.width/2; x = x + 1 ){
+   for(let y = 0; y < img3.height; y = y + 1){
+      let i = (x + y*img3.width)*4;
+
+      let pixr = img3.pixels[i+0];
+      let pixg = img3.pixels[i+1];
+      let pixb = img3.pixels[i+2];
+      let pixa = img3.pixels[i+3];
+
+       img3.pixels[i+2] = 0;
+
+ }
+}
+
+img3.updatePixels();
 }
