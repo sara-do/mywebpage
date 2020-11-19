686d3e5cd3049e2fff044bb313afb74f95580813
Sara Do <sara@Jiui-MacBook-Pro.local>
2020-11-19 18:41:32 +0900

    project 3_2

--- Project3_1/sketch.js
+++ Project3_1/sketch.js
@@ -21,45 +21,57 @@ function setup() {
 }
 
 function draw(){
-
+//offset settings
   xpos = noise(xoffset) * width;
   ypos = noise(yoffset) * height;
   xpos1 = noise(xoffset1) * 1100;
   ypos1 = noise(yoffset1) * height;
 
+//apricot coloured ellipse moving xpos
   fill(250,200,150);
   ellipse(xpos,100,120,220);
 
+//light yellow coloured ellipse moving ypos
   fill(252,250,152);
   ellipse(400, ypos, 100, 200);
 
+//Bright yellow coloured ellipse
   fill(300,300,100);
   ellipse(700, 180, 200, 300);
 
+//light green coloured ellipse
   fill(100,200,100);
   ellipse(280, 400, 100, 200);
 
+//light blue coloured ellipse moving xpos (using xpos1)
   fill(50,200,300);
   ellipse(xpos1,500,300,100);
 
+//bright purple coloured circle moving ypos (using ypos1)
   fill(100,50,200);
   circle(1100,ypos1,150,150);
 
+//light purple coloured circle
   fill(130,70,200);
   circle(900,400,100,100);
 
+// violet coloured circle
   fill(190,80,200);
   circle(100,300,80,100);
 
+//darker green coloured long ellipse
   fill(100,190,50);
   ellipse(600,300,300,20);
 
+//light orange coloured ellipse
   fill(300,190,50);
   ellipse(600,600,150,40);
 
+//bright orange coloured ellipse
   fill(300,120,50);
   ellipse(900,630,200,20);
 
+//navy coloured ellipse
   fill(40,30,90);
   ellipse(100,600,30,130);
 
--- Project3_2/sketch.js
+++ Project3_2/sketch.js
@@ -1,9 +1,54 @@
+let xoffset;
+let yoffset;
 
+let bcol;
+let rcol;
 
 function setup() {
+createCanvas(windowWidth,windowHeight);
+background(0,0,0);
+
+xoffset = 0.3;
+yoffset = 0.3;
 
 }
 
 function draw(){
+
+  bcol = noise(xoffset) * 255;
+  rcol = noise(yoffset) * 255;
+
+  fill(0,0,bcol,70);
+  rect(35, 81, 30, 50);
+
+  fill(30,0,bcol,70);
+  rect(100, 81, 50, 100);
+
+  fill(0,70,bcol,70);
+  rect(200, 81, 70, 200);
+
+  fill(0,0,bcol,120);
+  rect(320, 81, 80, 300);
+
+  fill(0,0,bcol,170);
+  rect(440, 81, 100, 400);
+
+  fill(0,20,bcol,10);
+  rect(580, 81, 120, 500);
+
+  fill(rcol,0,50,10);
+  rect(740, 81, 140, 600);
+
+  fill(rcol,0,40,170);
+  rect(920, 81, 120, 500);
+
+  fill(rcol,0,90,170);
+  rect(1080, 81, 100, 400);
+
+  fill(rcol,30,40,170);
+  rect(1220, 81, 80, 300);
+
+  xoffset = xoffset + 0.01;
+  yoffset = yoffset + 0.01;
 
 }
